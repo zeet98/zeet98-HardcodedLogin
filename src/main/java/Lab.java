3c2c97c395935be1579e13cdb2f456707a258fe1
Zeshan Tawa <zeshan.tawa98@gmail.com>
2023-02-23 20:34:33 +0000

    Completed Harcodedlogin assignment

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -15,6 +15,11 @@ public class Lab {
      * @return true if there is a successful login, and false otherwise.
      */
     public boolean login(String username, String password){
+        if((username == "admin") & (password == "qwerty")){
+            return true;
+        } else if((username == "user") & (password == "password")){
+            return true;
+        }
         return false;
     }
 }
